ddd17023838476cedff326e8975cc2fee6da40ad
Codeurdu34 <alexandre.lab@hotmail.fr>
2025-03-05 11:18:02 +0100

    Ajout de route Put facture  en BDD

--- BDD2/routes/facture_routes.py
+++ BDD2/routes/facture_routes.py
@@ -1,4 +1,7 @@
-from flask import Blueprint, jsonify
+from flask import Blueprint, jsonify, request
+from datetime import datetime
+import sys
+import os
 from models.database import db
 from models.facture import Facture
 from sqlalchemy.orm import Session
@@ -41,3 +44,24 @@ def get_facture(facture_id):
     if facture:
         return jsonify(facture.to_dict()), 200
     return jsonify({"error": "Facture not found"}), 404
+
+@facture_routes.route("/api/factures", methods=["POST"])
+def create_facture():
+    data = request.get_json()
+
+    
+    if not all(k in data for k in ("nom_client", "montant", "date", "status")):
+        return jsonify({"error": "Données incomplètes"}), 400
+
+    try:
+        nouvelle_facture = Facture(
+            nom_client=data["nom_client"],
+            montant=float(data["montant"]),
+            date=datetime.strptime(data["date"], "%Y-%m-%d").date(),
+            status=data["status"]
+        )
+        db.session.add(nouvelle_facture)
+        db.session.commit()
+        return jsonify(nouvelle_facture.to_dict()), 201
+    except Exception as e:
+        return jsonify({"error": str(e)}), 500
